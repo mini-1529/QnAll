e2cd641f42a52b2480a5d70c2927170a2b09132c
pihsdneirf <135335383+pihsdneirf@users.noreply.github.com>
2023-08-12 23:01:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,14 @@ if (receivedAccessToken) {
   setAccessTokenToCookie(receivedAccessToken, tokenExpirationTime);
 }
 
+function navigateToChatPage(postId) {
+  // 채팅 페이지 URL 생성
+  const chatPageURL = `https://example.com/chat?postId=${postId}`;
+  
+  // 새 창 또는 현재 창으로 페이지 이동
+  window.location.href = chatPageURL;
+}
+
 // 유저 정보를 서버에서 받아오는 함수
 function fetchUserData() {
   // AJAX 요청
@@ -96,7 +104,7 @@ function fetchDataBySubject(selectedOption) {
             // 게시판 항목 클릭 시 이벤트 추가
             const titleElement = eventItem.querySelector(".event-title");
             titleElement.addEventListener("click", function () {
-            handlePostClick(filteredData.id);
+            navigateToChatPage(filteredData.id); // 클릭한 게시물의 ID를 채팅 페이지로 전달
 
             eventList.appendChild(eventItem);
           });
